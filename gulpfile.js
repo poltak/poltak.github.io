2588a1a235e2f515fd1c38c916ee1c515f02a5b0
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-03-26 19:04:26 +0700

    Added Gulp tasks for images and JS
    - Removed Gruntfile
    - Added dev deps to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,58 @@
-var gulp = require('gulp');
+// Global plugins
+var gulp        = require('gulp');
+var cache       = require('gulp-cache');
+var concat      = require('gulp-concat');
 
 // Constants
-var CSS_FILES_INPUT_PATH = 'assets/css/src/**/*.+(scss|css)';
-var CSS_FILES_OUTPUT_PATH = 'assets/css/build/';
-var FINAL_CSS_FILE = 'master.min.css';
+var CSS_FILES_INPUT_PATH    = 'assets/css/src/**/*.+(scss|css)';
+var CSS_FILES_OUTPUT_PATH   = 'assets/css/build/';
+var JS_FILES_INPUT_PATH     = 'assets/js/src/**/*.js';
+var JS_FILES_OUTPUT_PATH    = 'assets/js/build/';
+var IMAGE_FILES_INPUT_PATH  = 'assets/img/src/**/*.+(jpg|png|gif|svg)';
+var IMAGE_FILES_OUTPUT_PATH = 'assets/img/build/';
+var FINAL_CSS_FILE          = 'master.min.css';
+var FINAL_JS_FILE           = 'master.min.js';
 
 
-gulp.task('styles', function() {
+// Task to handle concat and minification of CSS
+gulp.task('css', function() {
   var sass       = require('gulp-sass');
   var postcss    = require('gulp-postcss');
-  var sourcemaps = require('gulp-sourcemaps');
   var cssnano    = require('gulp-cssnano');
-  var concat     = require('gulp-concat');
 
-  gulp.src(CSS_FILES_INPUT_PATH)
-      .pipe( sass().on('error', sass.logError) )
-      .pipe( postcss([ require('autoprefixer'), require('precss') ]) )
-      .pipe( cssnano() )
-      .pipe( concat(FINAL_CSS_FILE) )
-      .pipe( gulp.dest(CSS_FILES_OUTPUT_PATH) );
+  return gulp.src(CSS_FILES_INPUT_PATH)
+             .pipe( sass().on('error', sass.logError) )
+             .pipe( postcss([ require('autoprefixer'), require('precss') ]) )
+             .pipe( cssnano() )
+             .pipe( concat(FINAL_CSS_FILE) )
+             .pipe( gulp.dest(CSS_FILES_OUTPUT_PATH) );
 });
 
-// Watch task
+
+// Task to handle concat and minifcation of JS
+gulp.task('js', function() {
+  var uglify = require('gulp-uglify');
+
+  return gulp.src(JS_FILES_INPUT_PATH)
+             .pipe( uglify() )
+             .pipe( concat(FINAL_JS_FILE) )
+             .pipe( gulp.dest(JS_FILES_OUTPUT_PATH) );
+});
+
+
+// Task to handle compressing of images
+gulp.task('images', function() {
+  var imagemin = require('gulp-imagemin');
+
+  return gulp.src(IMAGE_FILES_INPUT_PATH)
+             .pipe( cache(imagemin()) )
+             .pipe( gulp.dest(IMAGE_FILES_OUTPUT_PATH) );
+});
+
+
+// Default watch task for all above tasks
 gulp.task('default', function() {
-  gulp.watch(CSS_FILES_INPUT_PATH, ['styles']);
+  gulp.watch(CSS_FILES_INPUT_PATH, ['css']);
+  gulp.watch(JS_FILES_INPUT_PATH, ['js']);
+  gulp.watch(IMAGE_FILES_INPUT_PATH, ['images']);
 });
