f350d7e3c7399a66c691b8748a33580f4aaa5637
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-01-31 22:34:40 +0700

    Rewrite gulp file as es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,63 @@
-// Global plugins
-var gulp        = require('gulp');
-var cache       = require('gulp-cache');
-var concat      = require('gulp-concat');
-var sourcemaps  = require('gulp-sourcemaps');
+/* eslint import/no-extraneous-dependencies: 0, import/no-unresolved: 0, global-require: 0 */
+const gulp = require('gulp');
+const cache = require('gulp-cache');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
+// CSS Stuff
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('gulp-cssnano');
+const postcssPlugins = [require('autoprefixer'), require('precss')];
 
-// Constants
-var CSS_FILES_INPUT_PATH    = 'assets/css/src/**/*.+(scss|css)';
-var CSS_FILES_OUTPUT_PATH   = 'assets/css/build/';
-var JS_FILES_INPUT_PATH     = 'assets/js/src/**/*.js';
-var JS_FILES_OUTPUT_PATH    = 'assets/js/build/';
-var IMAGE_FILES_INPUT_PATH  = 'assets/img/src/**/*.+(jpg|png|gif|svg)';
-var IMAGE_FILES_OUTPUT_PATH = 'assets/img/build/';
-var FINAL_CSS_FILE          = 'master.min.css';
-var FINAL_JS_FILE           = 'master.min.js';
+// JS Stuff
+const uglify = require('gulp-uglify');
 
+// Image stuff
+const imagemin = require('gulp-imagemin');
+
+// Paths
+const config = {
+  css: {
+    input: 'assets/css/src/**/*.+(scss|css)',
+    output: 'assets/css/build/',
+    bundle: 'master.min.css',
+  },
+  js: {
+    input: 'assets/js/src/**/*.js',
+    output: 'assets/js/build/',
+    bundle: 'master.min.js',
+  },
+  img: {
+    input: 'assets/img/src/**/*.+(jpg|png|gif|svg)',
+    output: 'assets/img/build/',
+  },
+};
 
 // Task to handle concat and minification of CSS
-gulp.task('css', function() {
-  var sass       = require('gulp-sass');
-  var postcss    = require('gulp-postcss');
-  var cssnano    = require('gulp-cssnano');
-
-  return gulp.src(CSS_FILES_INPUT_PATH)
-             .pipe( sass().on('error', sass.logError) )
-             .pipe( postcss([ require('autoprefixer'), require('precss') ]) )
-             .pipe( cssnano() )
-             .pipe( concat(FINAL_CSS_FILE) )
-             .pipe( gulp.dest(CSS_FILES_OUTPUT_PATH) );
-});
-
+gulp.task('css', () => gulp.src(config.css.input)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss(postcssPlugins))
+  .pipe(cssnano())
+  .pipe(concat(config.css.bundle))
+  .pipe(gulp.dest(config.css.output)));
 
 // Task to handle concat and minifcation of JS
-gulp.task('js', function() {
-  var uglify = require('gulp-uglify');
-
-  return gulp.src(JS_FILES_INPUT_PATH)
-             .pipe( sourcemaps.init() )
-             .pipe( uglify() )
-             .pipe( concat(FINAL_JS_FILE) )
-             .pipe( sourcemaps.write() )
-             .pipe( gulp.dest(JS_FILES_OUTPUT_PATH) );
-});
-
+gulp.task('js', () => gulp.src(config.js.input)
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(concat(config.js.bundle))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.js.output)));
 
 // Task to handle compressing of images
-gulp.task('images', function() {
-  var imagemin = require('gulp-imagemin');
-
-  return gulp.src(IMAGE_FILES_INPUT_PATH)
-             .pipe( cache(imagemin()) )
-             .pipe( gulp.dest(IMAGE_FILES_OUTPUT_PATH) );
-});
-
+gulp.task('images', () => gulp.src(config.img.input)
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest(config.img.output)));
 
 // Default watch task for all above tasks
-gulp.task('default', function() {
-  gulp.watch(CSS_FILES_INPUT_PATH, ['css']);
-  gulp.watch(JS_FILES_INPUT_PATH, ['js']);
-  gulp.watch(IMAGE_FILES_INPUT_PATH, ['images']);
+gulp.task('default', () => {
+  gulp.watch(config.css.input, ['css']);
+  gulp.watch(config.js.input, ['js']);
+  gulp.watch(config.img.input, ['images']);
 });
