6e052bfa0f348f5ac3b63be060930185181ed230
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-01-01 21:48:15 +0700

    Add script loading support to main layout
    - some states in there which I'll maybe use them later for something

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,27 +1,60 @@
 import React from 'react'
+import loadScript from 'simple-load-script'
 
 import { Header, Footer, Nav } from '../components'
 import styles from './main.module.css'
 
-const MainLayout = ({
-    headerText,
-    subHeaderText,
-    backgroundImgSrc,
-    children,
-    calcYear = () => new Date().getFullYear(),
-}) => (
-    <div className={styles.main}>
-        <Nav />
-        <Header subText={subHeaderText} imgSrc={backgroundImgSrc}>
-            {headerText}
-        </Header>
+class MainLayout extends React.Component {
+    static defaultProps = {
+        extScripts: [
+            'https://use.fontawesome.com/releases/v5.6.3/js/solid.js',
+            'https://use.fontawesome.com/releases/v5.6.3/js/fontawesome.js',
+        ],
+    }
 
-        {children}
+    state = {
+        scriptsLoaded: false,
+        scriptsError: null,
+    }
 
-        <Footer>
-            This website's content are ©{calcYear()} Jonathan Poltak Samosir
-        </Footer>
-    </div>
-)
+    async componentDidMount() {
+        const scriptLoadPs = this.props.extScripts.map(src =>
+            loadScript(src, { inBody: true }),
+        )
+
+        try {
+            await Promise.all(scriptLoadPs)
+            this.setState(state => ({ scriptsLoaded: true }))
+        } catch (err) {
+            this.setState(state => ({ scriptsLoaded: true, scriptsError: err }))
+        }
+    }
+
+    render() {
+        const {
+            headerText,
+            subHeaderText,
+            backgroundImgSrc,
+            children,
+            calcYear = () => new Date().getFullYear(),
+        } = this.props
+
+        return (
+            <div className={styles.main}>
+                <Nav />
+                <Header subText={subHeaderText} imgSrc={backgroundImgSrc}>
+                    {headerText}
+                </Header>
+
+                {children}
+
+                <Footer>
+                    This website's content are ©{calcYear()} Jonathan Poltak
+                    Samosir
+                </Footer>
+            </div>
+        )
+    }
+}
 
 export default MainLayout
