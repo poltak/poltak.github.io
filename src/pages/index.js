f04f179f6635b555bc74d5f3216b655cb4309980
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-04-18 18:06:39 +0700

    Write content for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Header, List, Button, Icon } from 'semantic-ui-react'
+import { Link } from 'gatsby'
 
 import { MainLayout } from '../layouts'
 
@@ -9,24 +11,65 @@ const IndexPage = props => (
         title="poltak.github.io"
         {...props}
     >
+        <Header as="h2">What is this site?</Header>
+        <p>This site is an online space for myself to:</p>
+        <List bulleted>
+            <List.Item>
+                occasionally write about things I find interesting
+            </List.Item>
+            <List.Item>keep an online version of my resume updated</List.Item>
+            <List.Item>
+                talk a little bit about myself (for those who are interested)
+            </List.Item>
+            <List.Item>
+                play around with static site technologies, when I have spare
+                time
+            </List.Item>
+        </List>
+        <Header as="h2">Technologies used</Header>
         <p>
-            From Richard Hamming’s classic and must-read talk, “
-            <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-                You and Your Research
-            </a>
-            ”.
+            This site was created with the assistance of the following
+            technologies:
         </p>
-        <blockquote>
-            <p>
-                There is indeed an element of luck, and no, there isn’t. The
-                prepared mind sooner or later finds something important and does
-                it. So yes, it is luck.{' '}
-                <em>
-                    The particular thing you do is luck, but that you do
-                    something is not.
-                </em>
-            </p>
-        </blockquote>
+        <List bulleted>
+            <List.Item>
+                <Link to="https://pages.github.com">GitHub Pages</Link> - free
+                hosting and version control
+            </List.Item>
+            <List.Item>
+                <Link to="https://www.gatsbyjs.org/">GatsbyJS</Link> - allows me
+                to write my site in React and handles the static site build
+                process
+            </List.Item>
+            <List.Item>
+                <Link to="https://react.semantic-ui.com/">
+                    Semantic UI React
+                </Link>{' '}
+                - very nice UI framework with React integration
+            </List.Item>
+        </List>
+        <Header as="h2">Open source</Header>
+        <p>
+            The source code for this site can be found in my site’s{' '}
+            <Link to="https://github.com/poltak/poltak.github.io">
+                GitHub repo
+            </Link>
+            .
+        </p>
+        <p>
+            Feel free to criticize my code, send PRs, etc. I'm still learning
+            everyday, and always open to suggestions.
+        </p>
+
+        <Button
+            icon
+            color="blue"
+            labelPosition="left"
+            href="https://github.com/poltak/poltak.github.io/issues/new"
+            target="_blank"
+        >
+            <Icon name="discussions" /> Leave me feedback
+        </Button>
     </MainLayout>
 )
 
