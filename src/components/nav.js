47d7706340db738879ad234fee5ab352f09dc96a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-04-18 17:24:53 +0700

    Move from StaticQuery to react hook version

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,37 +1,34 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { Menu } from 'semantic-ui-react'
 
 import NavItem from './nav-item'
 
-const Nav = ({ pages }) => (
-    <StaticQuery
-        query={graphql`
-            query SiteTitleQuery {
-                site {
-                    siteMetadata {
-                        pages {
-                            name
-                            href
-                            icon
-                            header
-                        }
+const Nav = props => {
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    pages {
+                        name
+                        href
+                        icon
+                        header
                     }
                 }
             }
-        `}
-        render={data => {
-            const { pages } = data.site.siteMetadata
+        }
+    `)
 
-            return (
-                <Menu stackable fluid widths={pages.length}>
-                    {pages.map((page, i) => (
-                        <NavItem key={i} {...page} />
-                    ))}
-                </Menu>
-            )
-        }}
-    />
-)
+    const { pages } = data.site.siteMetadata
+
+    return (
+        <Menu stackable fluid widths={pages.length}>
+            {pages.map((page, i) => (
+                <NavItem key={i} {...props} {...page} />
+            ))}
+        </Menu>
+    )
+}
 
 export default Nav
