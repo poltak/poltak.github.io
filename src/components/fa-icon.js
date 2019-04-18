f599ef9ac5c0403ad9e0d84342c37b8706a23334
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-04-18 15:55:03 +0700

    Remove font awesome
    - semantic UI has their own icons which i can use

--- src/components/fa-icon.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const FAIcon = ({ name }) => <i className={`fas fa-${name}`} />
-
-export default FAIcon
--- src/static/external.js
+++ src/static/external.js
@@ -1,4 +1,1 @@
-export const scripts = [
-    'https://use.fontawesome.com/releases/v5.6.3/js/solid.js',
-    'https://use.fontawesome.com/releases/v5.6.3/js/fontawesome.js',
-]
+export const scripts = []
