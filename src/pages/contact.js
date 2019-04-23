1c763a5010f3ee0d6e3a2d8f2ec215763de856f4
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-04-23 18:45:45 +0700

    Write contact page stuff

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import MainLayout, { imageSelector } from '../layouts/main'
+import { ContactCard } from '../components/contact-card-container'
 
 const ContactPage = props => (
     <MainLayout
@@ -12,23 +13,10 @@ const ContactPage = props => (
         {...props}
     >
         <p>
-            From Richard Hamming’s classic and must-read talk, “
-            <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-                You and Your Research
-            </a>
-            ”.
+            My preferred means of contact is via email, although you can also
+            contact me through online social networks that I'm on.
         </p>
-        <blockquote>
-            <p>
-                There is indeed an element of luck, and no, there isn’t. The
-                prepared mind sooner or later finds something important and does
-                it. So yes, it is luck.{' '}
-                <em>
-                    The particular thing you do is luck, but that you do
-                    something is not.
-                </em>
-            </p>
-        </blockquote>
+        <ContactCard centered />
     </MainLayout>
 )
 
