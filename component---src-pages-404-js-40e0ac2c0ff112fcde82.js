abca3e0075d777ff2ab020bff812c6c560e90af6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-06-16 14:35:26 +0700

    Remove everything

--- component---src-pages-404-js-40e0ac2c0ff112fcde82.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n),t.d(n,"query",(function(){return u}));var a=t("q1tI"),c=t.n(a),r=t("aQu0"),o=t("YPLI");n.default=function(e){return c.a.createElement(o.a,Object.assign({backgroundImgSrc:Object(o.b)(e.data)},e),c.a.createElement(r.a,{as:"h2",textAlign:"center"},"Page ",c.a.createElement("code",null,e.location.pathname)," not found..."))};var u="4151434086"}}]);
-//# sourceMappingURL=component---src-pages-404-js-40e0ac2c0ff112fcde82.js.map
