03bd6dd8f71a167aa5a1ae3379aa45cff6eb3936
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2025-03-17 10:29:16 +0700

    Add dummy route

---
+++ src/routes/fun/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class="main-content">
+    <h1>ePUB Speed Reader</h1>
+</div>
+
+<style>
+    .main-content {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+</style>
