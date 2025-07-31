f63aa1048104f471b365e91d3224ea694e1a803a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2025-07-31 14:01:06 +0700

    Implement base loss function

--- src/lib/linear-classifier/core-functions.test.ts
+++ src/lib/linear-classifier/core-functions.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect, beforeEach, vi } from 'vitest'
 import * as coreFunctions from './core-functions.js'
 
-describe('linearClassifier', () => {
+describe('linear classifier', () => {
     test('should throw an error if the input vector length does not match the weight matrix column length', () => {
         const inputVector = [1, 2, 3]
         const weights = [
@@ -35,5 +35,37 @@ describe('linearClassifier', () => {
 
         const result = coreFunctions.linearClassifier(inputVector, weights, bias)
         expect(result).toEqual([14, 32])
+    })
+})
+
+describe('loss function', () => {
+    test('should throw an error if the correct class index is out of bounds', () => {
+        const scores = [1, 2, 3]
+        expect(() => coreFunctions.calculateLoss(scores, 4)).toThrow()
+        expect(() => coreFunctions.calculateLoss(scores, -4)).toThrow()
+        expect(() => coreFunctions.calculateLoss([], 0)).toThrow()
+    })
+
+    test('should calculate the correct loss', () => {
+        const scores = [1, 2, 3]
+        expect(coreFunctions.calculateLoss(scores, 0)).toBe(5)
+        expect(coreFunctions.calculateLoss(scores, 1)).toBe(2)
+        expect(coreFunctions.calculateLoss(scores, 2)).toBe(0)
+    })
+})
+
+describe('linear classifier with loss function', () => {
+    test('should calculate the correct loss', () => {
+        const inputVector = [1, 2, 3]
+        const weights = [
+            [1, 2, 3],
+            [4, 5, 6],
+        ]
+        const bias = [1, 2]
+
+        const scores = coreFunctions.linearClassifier(inputVector, weights, bias)
+        expect(scores).toEqual([14, 32])
+        expect(coreFunctions.calculateLoss(scores, 0)).toBe(19) // max(0, 14 - 32 + 1)
+        expect(coreFunctions.calculateLoss(scores, 1)).toBe(0) // max(0, 32 - 14 + 1)
     })
 })
--- src/lib/linear-classifier/core-functions.ts
+++ src/lib/linear-classifier/core-functions.ts
@@ -32,3 +32,34 @@ export function linearClassifier(
 
     return classScores
 }
+
+/**
+ * @param scores - Vector of scores for each class. e.g., [14, 32]
+ * @param correctClassIndex - Index of the "correct" class. i.e., which of the classes is the one that the inputVector was expected to be in.
+ * @param margin
+ * @returns Total loss for input scores (how poor the weights that produced the input scores are at correctly classifying the input)
+ */
+export function calculateLoss(scores: number[], correctClassIndex: number, margin = 1): number {
+    // Validate inputs
+    if (correctClassIndex < 0 || correctClassIndex >= scores.length) {
+        throw new Error('Correct class index must be within scores vector bounds')
+    }
+
+    const correctClassScore = scores[correctClassIndex]
+
+    const classLosses = new Array<number>(scores.length).fill(0)
+    for (let i = 0; i < scores.length; i++) {
+        // Skip the correct class
+        if (i === correctClassIndex) {
+            continue
+        }
+
+        const loss = scores[i] - correctClassScore + margin
+        if (loss > 0) {
+            classLosses[i] = loss
+        }
+    }
+
+    const totalLoss = classLosses.reduce((a, b) => a + b, 0)
+    return totalLoss
+}
