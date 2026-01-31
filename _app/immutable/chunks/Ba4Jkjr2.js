4a5c407248674d7207e58c2057e8372ce9c39b98
goblin <goblin@Nguyens-MacBook-Pro.local>
2026-01-31 21:41:29 +0700

    Updates

---
+++ _app/immutable/chunks/Ba4Jkjr2.js
@@ -0,0 +1,1 @@
+import{ad as o,ae as t,l as c,u}from"./BeLyubsk.js";function a(n){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(n){t===null&&a(),c&&t.l!==null?l(t).m.push(n):o(()=>{const e=u(n);if(typeof e=="function")return e})}function l(n){var e=n.l;return e.u??(e.u={a:[],b:[],m:[]})}export{r as o};
